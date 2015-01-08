f64bf15c77d8892fdb481be596df62e059860fe4
Greg Berger <greg@3kd.be>
2015-01-08 21:33:48 +0100

    using composer.json, REquest and Response classes

--- index.php
+++ index.php
@@ -6,23 +6,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
+$request = Request::createFromGlobals();
+
+$uri = $request->getPathInfo();
 
 
-// charge et initialise les bibliothèques globales
-// require_once 'model.php';
-// require_once 'controllers.php';
+if ('/' == $uri) {
 
-// route la requête en interne
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	$response = list_action();
 
-if (strstr($uri, '/index.php')) {
-	list_action();
-}elseif (strstr($uri, '/index.php/show') && isset($_GET['id'])) {
-	show_action($_GET['id']);
+}elseif ('/show' == $uri && $request->query->has('id') ) {
+	$response = show_action($request->query->get('id'));
 } else {
 
-	header('Status: 404 Not Found');
 
-	echo '<html><body><h1>Page Not Found</h1></body></html>';
+
+	$html = '<html><body><h1>Page Not Found</h1></body></html>';
+
+	$response = new Response($html, Response::HTTP_NOT_FOUND);
 
 }
+
+$response->send();
