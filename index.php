895aefa69ffc9dde9d229b689f1d6bffede1db56
Greg Berger <greg@3kd.be>
2015-01-08 21:19:30 +0100

    before using request class

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <?php
-// index.php
+// REéUPéRATION DE L'AUTOLOADER
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
 
 // charge et initialise les bibliothèques globales
-require_once 'model.php';
-require_once 'controllers.php';
+// require_once 'model.php';
+// require_once 'controllers.php';
 
 // route la requête en interne
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ('/ISL/2014-12-18_symfony/index.php' == $uri) {
+if (strstr($uri, '/index.php')) {
 	list_action();
-} elseif ('/ISL/2014-12-18_symfony/index.php/show' == $uri && isset($_GET['id'])) {
+}elseif (strstr($uri, '/index.php/show') && isset($_GET['id'])) {
 	show_action($_GET['id']);
 } else {
+
 	header('Status: 404 Not Found');
+
 	echo '<html><body><h1>Page Not Found</h1></body></html>';
+
 }
